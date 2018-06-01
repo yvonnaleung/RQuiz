52786f1bb7d7968efdb627454e1674c05c7a2af4
Yvonna Leung <yvonna.leung@gmail.com>
2018-05-31 21:02:43 -0700

    The 2nd and 3rd functions here are correct. 1st and 4th are still in progress.

--- vectors.r
+++ vectors.r
@@ -3,12 +3,18 @@ middle.element <- function(v) {
   ## If v has length n, then return the element at index n/2
   ##   (rounding up to the nearest whole number)
   
-  v[0]
+        n <- length(v)
+        x <- floor(v[n/2]) #return middle element of an even length
+        y <- floor(v[n/2] + 1) #return middle element of an odd length
+        return(x)
+        return(y)
+        #put if else statement
 }
 
 evens <- function(v) {
   ## TODO: return the even elements of v
-  v
+        y <- v[which(v %% 2 == 0)]
+        return(y)
 }
 
 hypotenuse <- function(a, b) {
@@ -17,12 +23,15 @@ hypotenuse <- function(a, b) {
   
   ## This should work even if a and b are vectors, and should be applied element-wise.
   ## Raise an error if a and b are not the same length.
-  a
+        pythagorean <- sqrt(a^2 + b^2)
+        return(pythagorean)
 }
 
 reverse.evens <- function(v) {
   ## TODO: reverse the order of the even elements of v.
   ## Leave the odd elements in their place.
   ## For example, reverse.evens(c(1,2,3,4,5)) == c(1,4,3,2,5)
-  v
+        y <- rev(v[which(v %% 2 == 0)])
+        return(y)
+        #not done
 }
