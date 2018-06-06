af76f1b6275138ff58fce22fe559f36170a50961
Yvonna Leung <yvonna.leung@gmail.com>
2018-06-05 19:45:30 -0700

    solutions to strings.r

--- strings.r
+++ strings.r
@@ -2,16 +2,26 @@ capitalize.words <- function (s) {
   ## TODO: Capitalize the first letter of every word in 's'.
   ## For example, "hello, everyone in the world!" becomes
   ## "Hello, Everyone In The World!"
-  s
+        
+        #split the characters
+        splitWord <- strsplit(s, " ")[[1]]
+        #capitalize the first letter, then paste together
+        paste(toupper(substring(splitWord, 1,1)), substring(splitWord, 2),
+              sep="", collapse=" ")
+            
 }
 
 reverse.words <- function(s) {
   ## TODO: reverse the order of the words in 's'
   ## For example, "hello, world!" becomaes "world! hello,"
-  s
+        
+        #split the string, reverse, then paste together
+        paste(rev(strsplit(s, "\\s+")[[1]]), collapse= " ")
 }
 
 digit.sum <- function(n) {
   # TODO: return the sum of the digits of n
-  n
+        
+        x <- as.character(n) #convert to a character
+        sum(as.numeric(unlist(strsplit(x, "")))) #convert it back to numeric and sum it
 }
